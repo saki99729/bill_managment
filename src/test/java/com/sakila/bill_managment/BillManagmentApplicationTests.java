c94cf1676d3ebe4fad7c1574d1e0dfed8a623aa0
sakila sandanuwan <sakila@sierrath.lk>
2026-06-26 13:25:27 +0530

    Add test configuration and H2 database dependency for Bill Management tests

--- src/test/java/com/sakila/bill_managment/BillManagmentApplicationTests.java
+++ src/test/java/com/sakila/bill_managment/BillManagmentApplicationTests.java
@@ -2,12 +2,13 @@ package com.sakila.bill_managment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BillManagmentApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
