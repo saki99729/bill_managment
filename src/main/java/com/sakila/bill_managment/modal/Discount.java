4fb3ea247acac9bfd12f7a81a16bfa86b097cb9b
sakila sandanuwan <sakila@sierrath.lk>
2026-06-30 23:06:29 +0530

    Add Batch and Discount entities for bill management

---
+++ src/main/java/com/sakila/bill_managment/modal/Discount.java
@@ -0,0 +1,29 @@
+package com.sakila.bill_managment.modal;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Data
+@Getter
+@Setter
+@Table(name = "discount")
+public class Discount {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "batch_id")
+    private Batch batchId;
+
+    @Column(name = "discount_percentage", nullable = false)
+    private double discountPercentage;
+
+    @Column(name = "amount", nullable = false)
+    private double amount;
+}
